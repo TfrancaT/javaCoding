65ed3a08aa4c2c3c4b459df6ecb777f53b9a44b4
TfrancaT <talesfranca.97@gmail.com>
2023-08-26 18:16:45 -0300

    praticando objetos com consumo de carro por viagem

--- javaconcept/src/executandoPratica.java
+++ javaconcept/src/executandoPratica.java
@@ -3,5 +3,13 @@ public class executandoPratica {
         praticandoObjetos alunoUm = new praticandoObjetos();
         alunoUm.calcularMedia(6.7, 6.3);
         System.out.println("Media: " + alunoUm.imprimirMedia());
+
+        calcularViagem hyundaiCreta = new calcularViagem();
+        hyundaiCreta.simularCusto(112, 7.1);
+        System.out.println( "O Hyundai Creta gastara " + hyundaiCreta.mostrarCusto() + "R$" );
+        
+        calcularViagem hondaCivic = new calcularViagem();
+        hondaCivic.simularCusto(112, 12.8);
+        System.out.println( "O Honda Civic gasta " + hondaCivic.mostrarCusto() + "R$" );
     }
 }
