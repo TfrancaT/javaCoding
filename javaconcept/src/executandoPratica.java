24b6338fbce680ed66dd5920b394aa8b1a175257
TfrancaT <talesfranca.97@gmail.com>
2023-08-26 17:31:03 -0300

    praticando objetos em casa

---
+++ javaconcept/src/executandoPratica.java
@@ -0,0 +1,7 @@
+public class executandoPratica {
+    public static void main(String[] args) {
+        praticandoObjetos alunoUm = new praticandoObjetos();
+        alunoUm.calcularMedia(6.7, 6.3);
+        System.out.println("Media: " + alunoUm.imprimirMedia());
+    }
+}
