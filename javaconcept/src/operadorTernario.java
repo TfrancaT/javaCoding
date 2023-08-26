e71ccea0864f90e5fd47e7988884155825385516
TfrancaT <talesfranca.97@gmail.com>
2023-08-26 17:21:24 -0300

    estruturando operador ternario

--- javaconcept/src/operadorTernario.java
+++ javaconcept/src/operadorTernario.java
@@ -6,6 +6,11 @@ public class operadorTernario {
         numberA = 6;
         numberB = 5;
 
+        String primeiroNome = "Tales";
+        String segundoNome = "Tales";
+
+        System.out.println(primeiroNome.equals(segundoNome));
+
         
         // Utilizando operador ternário para simplificar uma condição de checagem entre duas variavéis;
         String numberResult = numberA == numberB ?"true" : "false";
